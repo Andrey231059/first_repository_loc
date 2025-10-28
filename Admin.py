79c82f9a66e6d8611820e85e62c7276dff1a3542
Andrey231059 <iusov.andrey@yandex.ru>
2025-10-28 09:22:21 +0300

    Update Admin.py

--- Admin.py
+++ Admin.py
@@ -1,46 +1,52 @@
 class User():
-    def __init__(self, ID, name, access_level):
-        self.__private_ID = ID
-        self.__private_name = name
-        self.__private_access_level = access_level
+    def __init__(self, user_id, name, access_level):
+        self.user_id = user_id
+        self._name = name
+        self._access_level = access_level
+
+    def get_user_ID(self):
+        return self.user_id
+
+    def get_name(self):
+        return self._name
+
+    def get_access_level(self):
+        return self._access_level
 
 
 class Admin(User):
-    def __init__(self, ID, name, access_level,admin_rights):
-        super().__init__(ID, name, access_level)
-        self.admin_rights = admin_rights
-        self.users = []
+    def __init__(self, user_id, name, access_level,admin_rights):
+        super().__init__(user_id, name, access_level)
+        self._admin_rights = admin_rights
+        self._users = []
 
-    def get_users(self):
-        return self.users
+    def add_user(self, user_id, name, access_level):
+        new_user = User(user_id, name, access_level)
+        self._users.append(new_user)
 
-    def add_user(self):
-        name = input("Введите имя пользователя - ")
-        ID = input("Введите ID - ")
-        access_level = int(input("Введите права пользотателя. От 1 до 5 - "))
-        self.users.append({"ID": ID, "name": name, "access_level": access_level, "admin_rights": False})
-
-    def remove_user(self):
-        name = input("Введите имя пользователя - ")
-        for t in self.users:
-            if t["name"] == name:
-                self.users.remove(t)
+    def remove_user(self, name):
+       for user in self._users:
+            if user.get_name() == name:
+                self._users.remove(user)
                 print(f"Пользователь {name} удалён.")
-                return
-        print(f"Пользователь {name} не найден.")
+       return
+       print(f"Пользователь {name} не найден.")
 
     def print_users(self):
-        for t in self.users:
-            print(f"пользователь - {t['name']} доступ - {t['access_level']}")
+        for user in self._users:
+            print(f"пользователь - {user.get_name()} доступ - {user.get_access_level()}")
 
 
-Ad = Admin(ID = "A001", name = "Админ", access_level = 5, admin_rights = True)
-Ad.add_user()
-Ad.add_user()
-Ad.add_user()
-Ad.print_users()
-Ad.remove_user()
-Ad.print_users()
+admin = Admin(user_id = "A001", name = "Админ", access_level = 5, admin_rights = True)
+
+admin.add_user(user_id = "U001", name = "Андрей", access_level = 2)
+admin.add_user(user_id = "U002", name = "Екатерина", access_level = 3)
+admin.add_user(user_id = "U003", name = "Алексей", access_level = 4)
+admin.print_users()
+admin.remove_user("Андрей")
+admin.print_users()
+
+
 
 
 
